280e4259f764910597be817635a153ac7fe5f31c
azdoolittle <drewdoolittle@gmail.com>
2020-06-03 12:11:45 -0400

    fixed imports. added respective html pages

--- src/main/java/com/example/demo/SecConfig.java
+++ src/main/java/com/example/demo/SecConfig.java
@@ -1,13 +1,18 @@
 package com.example.demo;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
 
 @Configuration
-public class SecConfig {
+public class SecConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity httpSecurity) throws Exception {
@@ -25,7 +30,7 @@ public class SecConfig {
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
         auth.jdbcAuthentication()
-                .dataSource(dataSource)
+//                .dataSource(dataSource)
                 .withDefaultSchema()
                 .withUser(User.withUsername("admin")
                     .password(passwordEncoder().encode("admin"))
@@ -33,5 +38,10 @@ public class SecConfig {
                 .withUser(User.withUsername("user")
                     .password(passwordEncoder().encode("user"))
                     .roles("user"));
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
