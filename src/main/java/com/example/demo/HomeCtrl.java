5e50b34f8a58a338054a57e91c69503ca84082ca
azdoolittle <drewdoolittle@gmail.com>
2020-06-04 13:44:29 -0400

    added register route and form page

--- src/main/java/com/example/demo/HomeCtrl.java
+++ src/main/java/com/example/demo/HomeCtrl.java
@@ -3,9 +3,15 @@ package com.example.demo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.validation.Valid;
 import java.security.Principal;
+import java.util.HashSet;
+import java.util.Set;
 
 @Controller
 public class HomeCtrl {
@@ -13,11 +19,39 @@ public class HomeCtrl {
     @Autowired
     UserRepository userRepository;
 
+    @Autowired
+    RoleRepository roleRepository;
+
     @RequestMapping("/secure")
     public String secure(Principal principal, Model model) {
         String username = principal.getName();
         model.addAttribute("user", userRepository.findByUsername(username));
         return "secure";
+    }
+
+    @RequestMapping("/register")
+    public String showRegistrationPage(Model model) {
+        model.addAttribute("user", new User());
+            return "register";
+    }
+
+    @PostMapping("/register")
+    public String processRegistrationPage(@Valid @ModelAttribute("user") User user, BindingResult result, Model model) {
+        model.addAttribute("user", user);
+        if (result.hasErrors()) {
+            return "register";
+        }
+        model.addAttribute("message", "User Account Created");
+
+        user.setEnabled(true);
+        Role role = new Role(user.getUsername(), "ROLE_USER");
+        Set<Role> roles = new HashSet<Role>();
+        roles.add(role);
+
+        roleRepository.save(role);
+        userRepository.save(user);
+
+        return "index";
     }
 
     @RequestMapping("/")
--- src/main/java/com/example/demo/SpringBoot406.java
+++ src/main/java/com/example/demo/SpringBoot406.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBoot405 {
+public class SpringBoot406 {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringBoot405.class, args);
+        SpringApplication.run(SpringBoot406.class, args);
     }
 
 }
